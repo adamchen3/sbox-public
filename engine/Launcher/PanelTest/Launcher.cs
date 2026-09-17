a4b09dfdf71e2ff465fd0cb0603faa6b1f6343f5
adamchen3 <adamchen3@outlook.com>
2026-09-18 00:13:00 +0800

    例子修改。

--- engine/Launcher/PanelTest/Launcher.cs
+++ engine/Launcher/PanelTest/Launcher.cs
@@ -22,8 +22,8 @@ public class PanelTestAppSystem : PanelAppSystem
     protected override void OnInitialized()
     {
 
-		window = new Editor.PanelWindow( "Welcome to the s&box editor", new Vector2( 1100, 660 ), new Vector2( -1, -1 ), borderless: true, vsync: true );
-		window.Root.AddChild( new RootPanel() );
+        window = new Editor.PanelWindow( "Welcome to the s&box editor", new Vector2( 1100, 660 ), new Vector2( -1, -1 ), borderless: true, vsync: true );
+        window.Root.AddChild( new RootPanel() );
     }
 
 }
@@ -34,18 +34,39 @@ class RootPanel : Panel
 
     public RootPanel()
     {
+
         StyleSheet.Parse( @"
             .backdrop {
                 position: absolute;
                 left: 0px;
                 top: 0px;
-                width: 100%;
+                width: 50%;
                 height: 100%;
                 background-color: green;
                 filter: blur( 8px ) saturate( 1.1 ) brightness( 0.45 );
             }
 
+            .backdrop2 {
+                position: absolute;
+                right: 0px;
+                top: 0px;
+                width: 50%;
+                height: 100%;
+                background-color: yellow;
+                // filter: blur( 8px ) saturate( 1.1 ) brightness( 0.45 );
+            }
+
             .box {
+                position: absolute;
+                width: 200px;
+                height: 200px;
+                background-color: blue;
+                // filter: saturate( 1.1 );
+            }
+
+            .box2 {
+                position: absolute;
+                right: 0px;
                 width: 200px;
                 height: 200px;
                 background-color: blue;
@@ -53,23 +74,42 @@ class RootPanel : Panel
             }
 
             .inner-box {
+                position: absolute;
                 width: 100px;
                 height: 100px;
                 background-color: red;
-                filter: brightness( 0.45 );
+                // filter: brightness( 0.45 );
+            }
+
+            .inner-box2 {
+                position: absolute;
+                right: 0px;
+                width: 100px;
+                height: 100px;
+                background-color: red;
+                // filter: brightness( 0.45 );
             }
             " );
 
         var backdrop = AddChild<Panel>();
         backdrop.AddClass( "backdrop" );
 
+        var backdrop2 = AddChild<Panel>();
+        backdrop2.AddClass( "backdrop2" );
+
         fpsLabel = Add.Label( "", "fps" );
 
         var box = AddChild<Panel>();
         box.AddClass( "box" );
 
-        var innerBox = box.AddChild<Panel>();
+        var box2 = AddChild<Panel>();
+        box2.AddClass( "box2" );
+
+        var innerBox = AddChild<Panel>();
         innerBox.AddClass( "inner-box" );
+
+        var innerBox2 = AddChild<Panel>();
+        innerBox2.AddClass( "inner-box2" );
     }
 
     int frameCount;
